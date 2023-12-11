d1b6512e1f5f333cc605ac2c8c8aef6c31d1614c
v-giorgio <vitorlcardoso17@gmail.com>
2023-12-11 12:10:24 -0300

    fix: corrections on php code

--- api/index.php
+++ api/index.php
@@ -1,3 +1,157 @@
+<?php
+const MSG_PRECO_A_VISTA_MENOR = "O preço à vista é menor do que o preço total corrigido → compre à vista";
+const MSG_PRECO_A_VISTA_IGUAL = "O preço à vista é igual ao preço total corrigido";
+
+function getConstantValues($t, $p, $hasEntrada) {
+    if ($hasEntrada) {
+        return [
+            pow((1 + $t), ($p - 2)),
+            pow((1 + $t), ($p - 1)),
+            pow((1 + $t), $p)
+        ];
+    }
+
+    return [
+        pow((1 + $t), (-$p)),
+        pow((1 + $t), (-$p - 1)),
+        0
+    ];
+}
+
+function getFt($x, $y, $t, $p, $hasEntrada) {
+    $constants = getConstantValues($t, $p, $hasEntrada);
+
+    if ($hasEntrada) {
+        return [
+            $y * $t * $constants[1] - ($x / $p * ($constants[2] - 1)),
+            $y * ($constants[1] + $t * ($p - 1) * $constants[0]) - $x * $constants[1]
+        ];
+    }
+
+    return [
+        $y * $t - (($x / $p) * (1 - $constants[0])),
+        $y - ($x * $constants[1])
+    ];
+}
+
+function calculateTax($x, $y, $p, $hasEntrada) {
+    $t0 = $x / $y;
+    $tn = 0;
+    $numberIter = 0;
+
+    while (abs($tn - $t0) > 0.0001) {
+        if ($tn !== 0) $t0 = $tn;
+        $ftftlinha = getFt($x, $y, $t0, $p, $hasEntrada);
+        $ft = $ftftlinha[0];
+        $ftlinha = $ftftlinha[1];
+        $tn = $t0 - ($ft / $ftlinha);
+        $numberIter++;
+    }
+
+    return [$tn * 100, $numberIter];
+}
+
+function calculateCF($t, $p) {
+    return $t / (1 - pow((1 + $t), (-$p)));
+}
+
+function calculateFactor($fe, $p, $CF) {
+    return $fe / ($p * $CF);
+}
+
+function calculateFe($t = 0) {
+    return 1 + $t;
+}
+
+function calculatePrecoFinal($x, $fe, $p, $CF) {
+    return $x * ($fe / ($p * $CF));
+}
+
+function getPercentualPagoAMais($A, $y) {
+    return (($A - $y) / $y) * 100;
+}
+
+function getDesconto($A, $y) {
+    return (($A - $y) / $A) * 100;
+}
+
+function getDataTabelaPrice($prestacao, $valorFinanciado, $tax, $p) {
+    $jurosPorMes = [];
+    $amortizacaoPorMes = [];
+    $saldoDevedorPorMes = [];
+
+    $novoValorFinanciado = $valorFinanciado;
+    $novaAmortizacao = 0.00;
+    $novoJuros = $tax;
+
+    // primeira linha com valores iniciais:
+    $saldoDevedorPorMes[] = $novoValorFinanciado;
+    $jurosPorMes[] = $tax;
+    $amortizacaoPorMes[] = $novaAmortizacao;
+
+    for ($i = 1; $i < $p; $i++) {
+        $novoJuros = $novoValorFinanciado * $tax;
+        $jurosPorMes[] = $novoJuros;
+
+        $novaAmortizacao = $prestacao - $novoJuros;
+        $amortizacaoPorMes[] = $novaAmortizacao;
+
+        $novoValorFinanciado -= $novaAmortizacao;
+        $saldoDevedorPorMes[] = $novoValorFinanciado;
+    }
+
+    return [
+        $jurosPorMes,
+        $amortizacaoPorMes,
+        $saldoDevedorPorMes
+    ];
+}
+
+function orquestrator($x, $y, $p, $hasEntrada, $tax = 0) {
+    $taxData = calculateTax($x, $y, $p, $hasEntrada);
+    $taxUpdated = $tax == 0 ? $taxData[0] : $tax;
+
+    $cf = calculateCF($taxUpdated, $p);
+    $fe = $hasEntrada ? calculateFe($taxUpdated) : calculateFe();
+    $factor = calculateFactor($fe, $p, $cf);
+    $precoTotalCorrigido = calculatePrecoFinal($x, $fe, $p, $cf);
+
+    $mensagemPreco = $y < $precoTotalCorrigido ? MSG_PRECO_A_VISTA_MENOR : MSG_PRECO_A_VISTA_IGUAL;
+
+    $jurosEmbutidos =  getPercentualPagoAMais($x, $y);
+    $desconto = getDesconto($x, $y);
+
+    $percentualPagoAMais = getPercentualPagoAMais($precoTotalCorrigido, $y);
+
+    $prestacao = $cf * $y;
+    $valorFinanciado = $y - $prestacao;
+
+    $tabelaPriceData = getDataTabelaPrice($prestacao, $valorFinanciado, $taxUpdated, $p);
+    $totalJurosTabelaPrice = $precoTotalCorrigido - $y;
+
+    $precoAVistaAtualizado = $prestacao * $p;
+
+    return [
+        $mensagemPreco,
+        number_format($cf, 4),
+        '$' . number_format($precoTotalCorrigido, 4),
+        number_format($taxData[0], 4),
+        $taxData[1],
+        number_format($factor, 4),
+        number_format($jurosEmbutidos, 2) . '%',
+        number_format($desconto, 2) . '%',
+        number_format($percentualPagoAMais, 2) . '%',
+        '$' . number_format($valorFinanciado, 2),
+        number_format($precoAVistaAtualizado, 2),
+        number_format($prestacao, 2),
+        array_map(fn ($j) => number_format($j, 2), $tabelaPriceData[0]),
+        number_format($totalJurosTabelaPrice, 2),
+        array_map(fn ($a) => number_format($a, 2), $tabelaPriceData[1]),
+        array_map(fn ($s) => number_format($s, 2), $tabelaPriceData[2]),
+    ];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -105,53 +259,6 @@
             border: 2px solid black;
         }
     </style>
-    
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            var hasEntradaCheckbox = document.getElementById("idp");
-            var hasEntradaInput = document.getElementById("hasEntrada");
-
-            hasEntradaCheckbox.addEventListener("change", function () {
-                hasEntradaInput.value = hasEntradaCheckbox.checked ? 1 : 0;
-            });
-
-            var calculateBtn = document.getElementById("submitButton");
-
-            calculateBtn.addEventListener("click", function (event) {
-                event.preventDefault();
-
-                // Obtenha os valores dos campos de entrada
-                var numParcelas = document.getElementById("parc").value;
-                var taxaJuros = document.getElementById("itax").value;
-                var valorFinanciado = document.getElementById("ipv").value;
-                var valorFinal = document.getElementById("ipp").value;
-                var valorAVoltar = document.getElementById("valoravoltar").value;
-                var mesesAVoltar = document.getElementById("mesesavoltar").value;
-                var hasEntrada = document.getElementById("hasEntrada").value;
-
-                // Crie um objeto FormData para enviar os dados ao servidor
-                var formData = new FormData();
-                formData.append("parc", numParcelas);
-                formData.append("itax", taxaJuros);
-                formData.append("ipv", valorFinanciado);
-                formData.append("ipp", valorFinal);
-                formData.append("valoravoltar", valorAVoltar);
-                formData.append("mesesavoltar", mesesAVoltar);
-                formData.append("idp", hasEntrada);
-
-                // Faça uma solicitação AJAX para o script PHP
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", "calculator.php", true);
-                xhr.onload = function () {
-                    if (xhr.status == 200) {
-                        // Exiba a resposta do servidor (resultado do PHP)
-                        console.log(xhr.responseText);
-                    }
-                };
-                xhr.send(formData);
-            });
-        });
-    </script>
 </head>
 <body>
     <fieldset id="cdcfieldset" class="main-box ui-widget-content">
@@ -159,16 +266,13 @@
             <strong>Crédito Direto ao Consumidor</strong>
         </legend>
         <form method="post">
-            <?php
-            $hasEntrada = 0;
-            ?>
             <div class="box">
                 <span class="input-group-addon" style="color: antiquewhite">$</span>
                 <label for="parc">Parcelamento:</label>
                 <input
                     id="parc"
                     type="number"
-                    name="np"
+                    name="parc"
                     size="5"
                     placeholder="12"
                     min="1"
@@ -182,7 +286,7 @@
                 <input
                     id="itax"
                     type="number"
-                    name="tax"
+                    name="itax"
                     size="10"
                     placeholder="0,05"
                     min="0.0"
@@ -196,7 +300,7 @@
                 <input
                     id="ipv"
                     type="number"
-                    name="pv"
+                    name="ipv"
                     min="0.0"
                     placeholder="200,00"
                     step="0.01"
@@ -209,7 +313,7 @@
                 <input
                     id="ipp"
                     type="number"
-                    name="pp"
+                    name="ipp"
                     placeholder="500,00"
                     min="0.0"
                     step="0.01"
@@ -221,7 +325,7 @@
                 <input
                     id="valoravoltar"
                     type="number"
-                    name="pp"
+                    name="valoravoltar"
                     placeholder="500,00"
                     min="0.0"
                     step="0.01"
@@ -233,7 +337,7 @@
                 <input
                     id="mesesavoltar"
                     type="number"
-                    name="pp"
+                    name="mesesavoltar"
                     placeholder="2"
                     min="0"
                     step="1"
@@ -241,74 +345,78 @@
                 /><br />
 
                 <label for="idp">Entrada?</label>
-                <input id="idp" type="checkbox" name="dp" <?php echo $hasEntrada == 1 ? 'checked' : ''; ?> /><br />
+                <input id="idp" type="checkbox" name="idp" /><br />
             </div>
             <div class="messages">
                 <input
                     id="submitButton"
                     class="button"
                     type="submit"
-                    value="Calcular"
+                    name="submitButton"
                 />
             </div>
         </form>
     </fieldset>
 
-    <main>
-        <ul class="list-info">
-            <?php
-            $valorFinanciado = 1000;
-            $taxaReal = 5.0;
-            $iteracoes = 10;
-            echo "
-                <li>Valor financiado: R$ " . number_format($valorFinanciado, 2) . "</li>
-                <li>Taxa Real: " . number_format($taxaReal, 2) . "%</li>
-                <li>Iterações: " . $iteracoes . "</li>
-            ";
-            ?>
-        </ul>
-    </main>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submitButton"])) {
+            $parcelamento = $_POST["parc"];
+            $taxaJuros = $_POST["itax"];
+            $valorFinanciado = $_POST["ipv"];
+            $valorFinal = $_POST["ipp"];
+            $valorAVoltar = isset($_POST["valoravoltar"]) ? $_POST["valoravoltar"] : "";
+            $mesesAVoltar = isset($_POST["mesesavoltar"]) ? $_POST["mesesavoltar"] : "";
+            $entrada = isset($_POST["idp"]) ? true : false;
+        
+            $valuesArr = orquestrator(floatval($valorFinanciado), floatval($valorFinal),
+            floatval($parcelamento), $entrada, floatval($taxaJuros));
 
-    <table class="table">
-        <h1>Tabela Price</h1>
-        <thead>
-            <tr>
-                <th scope="col">Mês</th>
-                <th scope="col">Prestação</th>
-                <th scope="col">Juros</th>
-                <th scope="col">Amortização</th>
-                <th scope="col">Saldo Devedor</th>
-            </tr>
-        </thead>
-        <tbody class="table-result">
-            <?php
-            for ($i = 1; $i <= 12; $i++) {
-                echo "
+            echo '
+            <main>
+                <ul class="list-info">
+                    <li>Valor financiado: R$ ' . $valuesArr[10] . '</li>
+                    <li>Taxa Real: ' . $valuesArr[3] . '%</li>
+                    <li>Iterações: ' . $valuesArr[4] . '</li>
+                </ul>
+            </main>
+        
+            <table class="table">
+                <h1>Tabela Price</h1>
+                <thead>
                     <tr>
-                        <td>$i</td>
-                        <td>R$ 100.00</td>
-                        <td>R$ 10.00</td>
-                        <td>R$ 90.00</td>
-                        <td>R$ 810.00</td>
+                        <th scope="col">Mês</th>
+                        <th scope="col">Prestação</th>
+                        <th scope="col">Juros</th>
+                        <th scope="col">Amortização</th>
+                        <th scope="col">Saldo Devedor</th>
                     </tr>
-                ";
-            }
-            ?>
-        </tbody>
-        <tfoot class="table-result-final">
-            <?php
-            $totalJuros = 120.0;
-            echo "
-                <tr>
-                    <td>Total</td>
-                    <td>R$ 1200.00</td>
-                    <td>R$ " . number_format($totalJuros, 2) . "</td>
-                    <td>R$ 880.00</td>
-                    <td>R$ 0.00</td>
-                </tr>
-            ";
-            ?>
-        </tfoot>
-    </table>
+                </thead>
+                <tbody class="table-result">';
+                for ($i = 0; $i < count($valuesArr[12]); $i++) {
+                    echo '
+                    <tr>
+                        <td>' . ($i + 1) . '</td>
+                        <td>R$ ' . $valuesArr[11] . '</td>
+                        <td>R$ ' . $valuesArr[12][$i] . '</td>
+                        <td>R$ ' . $valuesArr[14][$i] . '</td>
+                        <td>R$ ' . $valuesArr[15][$i] . '</td>
+                    </tr>
+                    ';
+                }
+                echo '
+                </tbody>
+                <tfoot class="table-result-final">
+                    <tr>
+                        <td>Total</td>
+                        <td>R$ ' . $valuesArr[10] . '</td>
+                        <td>R$ ' . $valuesArr[13] . '</td>
+                        <td>R$ ' . $valuesArr[2] . '</td>
+                        <td>R$ 0.00</td>
+                    </tr>
+                </tfoot>
+            </table>
+            ';
+        }
+    ?>
 </body>
 </html>
